e234b07f9367774f6bd7dcfc667edfc7508efc53
watarun <watarun54@gmail.com>
2018-10-06 00:12:23 +0900

    feature front

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ var db = require('./db');
 
 app.get('/', function(req, res, next) {
   db.pool.query('SELECT * FROM todos;', (err, results, fields)=>{
+      res.header("Access-Control-Allow-Origin", "*");
       if(err)
         return res.json(err)
       else
@@ -12,7 +13,9 @@ app.get('/', function(req, res, next) {
 })  
 
 app.get('/sample', function(req, res, next) {
-  res.json({ title: "aaa", name: "kkk"})
+  //res.header("Access-Control-Allow-Origin", "http://localhost:4000");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.json({"todos":[{"id":1,"title":"aaa","done":0,"created_at":"2018-10-04T12:42:45.000Z","updated_at":"2018-10-04T12:42:45.000Z"},{"id":2,"title":"buy onion","done":0,"created_at":"2018-10-04T12:43:45.000Z","updated_at":"2018-10-04T12:43:45.000Z"},{"id":3,"title":"study English","done":0,"created_at":"2018-10-04T12:45:51.000Z","updated_at":"2018-10-04T12:45:51.000Z"}]})
 })
 
 app.listen(3000);
--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import fetchJsonp from 'fetch-jsonp';
 import './App.css';
+import axios from "axios";
+
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+
+  async componentDidMount() {
+    const request = axios.create({
+      baseURL: 'http://localhost:3000/'
+    })
+    request.get('/')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          todos: res.data.todos
+        });
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>ToDo List</h1>
+        <ul>
+        {this.state.todos.map((todo, index) => (
+          <li key={index}>{todo.title}</li>
+        ))}
+        </ul>
       </div>
     );
   }
